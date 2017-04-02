0e12e8def58b1e164573580104efbe454d32ee58
Syanima <syanima@thoughtworks.com>
2017-04-02 23:47:32 +0530

    Implemented the multiple pipe operator

---
+++ README.txt
@@ -0,0 +1,4 @@
+1. Compile the C file using command 'gcc -o pipe pipe.c'
+
+2. Run the C filr using command './pipe'
+
---
+++ src/pipe.c
@@ -0,0 +1,50 @@
+#include <libgen.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
+void loop_pipe(char ***cmd) 
+{
+  int   p[2];
+  pid_t pid;
+  int   fd_in = 0;
+
+  while (*cmd != NULL)
+    {
+      pipe(p);
+      if ((pid = fork()) == -1)
+        {
+          exit(EXIT_FAILURE);
+        }
+      else if (pid == 0)
+        {
+          dup2(fd_in, 0); //change the input according to the old one 
+          if (*(cmd + 1) != NULL)
+            dup2(p[1], 1);
+          close(p[0]);
+          execvp((*cmd)[0], *cmd);
+          exit(EXIT_FAILURE);
+        }
+      else
+        {
+          wait(NULL);
+          close(p[1]);
+          fd_in = p[0]; //save the input for the next command
+          cmd++;
+        }
+    }
+}
+
+int main()
+{
+  char *ls[] = {"ls", NULL};
+  char *grep[] = {"grep", "pipe", NULL};
+  char *wc[] = {"wc", NULL};
+  char **cmd[] = {ls, grep, wc, NULL};
+
+  loop_pipe(cmd);
+  return (0);
+}
+
